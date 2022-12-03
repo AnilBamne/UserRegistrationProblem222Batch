b3ff3dc74011c738a7f503c73e90ffe96b80832a
AnilBamne <anilbamne20@gmail.com>
2022-12-03 18:23:52 +0530

    Adding:WelcomeMessage

---
+++ UserRegistraionProblems222Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistraionProblems222Batch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problems using Regex");
+        }
+    }
+}
