7fc0ed8ea18dbdaf0bcb1444e43fb32c43e0be5d
AnilBamne <anilbamne20@gmail.com>
2022-12-03 18:42:04 +0530

    Adding:UC1EnterAValidFirstName code

--- UserRegistraionProblems222Batch/Program.cs
+++ UserRegistraionProblems222Batch/Program.cs
@@ -7,6 +7,23 @@ namespace UserRegistraionProblems222Batch
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problems using Regex");
+            
+            Console.WriteLine("\nChoose UC to execute\n1:Validate first name\n2:Validate last name\n3:Validate Email");
+            int option=int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    ValidateFirstName.Validate();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
---
+++ UserRegistraionProblems222Batch/ValidateFirstName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistraionProblems222Batch
+{
+    public class ValidateFirstName
+    {
+        public static void Validate()
+        {
+            Console.WriteLine("Enter First Name");
+            string fName = Console.ReadLine();
+            string regex = "^[A-Z]{1}[a-zA-z]{2,}$";
+            bool result=Regex.IsMatch(fName, regex);
+            Console.WriteLine(result);
+        }
+    }
+}
